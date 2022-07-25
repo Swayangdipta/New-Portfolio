6b6699e2115d70a939d1c7032c0db3f710fbe329
Swayangdipta Das <swayangdiptacc@gmail.com>
2022-07-25 15:48:19 +0530

    Added Animation and Social Links Also some Css for responsiveness

---
+++ assets/js/parallax.js
@@ -0,0 +1,16 @@
+let controller = new ScrollMagic.Controller()
+let timeline = new TimelineMax()
+
+timeline
+    .to('.landing_img_bottom',3,{y: -200})
+    .to('.blank_div',3,{y: -300},'-=3')
+    .to('.scroll_down_wrapper',3,{y: -300},'-=3')
+    .to('.projects',3,{y: -300},'-=3')
+    .to('.about',3,{y: -300},'-=3')
+
+let scene = new ScrollMagic.Scene({
+    triggerElement: "",
+    duration: "100%",
+    triggerHook: 0,
+}).setTween(timeline)
+.addTo(controller)
